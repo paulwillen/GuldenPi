861fdfcbc716a1dc9b8472b586c4f93c63395a16
Paul Willen <paul@willen.net>
2017-01-09 11:17:08 +0100

    Adding Basic Download and Configuration settings...

--- bash/install.sh
+++ bash/install.sh
@@ -1,3 +1,22 @@
 #basic update
 sudo apt-get -y --force-yes update
 sudo apt-get -y --force-yes upgrade
+
+cd /
+
+sudo mkdir /guldenserver
+sudo wget https://developer.gulden.com/download/161/Gulden161-linux32.tar.xz -P /guldenserver
+
+sudo tar -xvf /guldenserver/Gulden161-linux32.tar.xz -C /guldenserver/Gulden
+sudo mkdir /guldenserver/datadir
+sudo touch /guldenserver/datadir/Gulden.conf
+
+
+echo "disablewallet=1" >> /guldenserver/datadir/Gulden.conf
+echo "maxconnections=20" >> /guldenserver/datadir/Gulden.conf
+
+uuiduser=$(uuidgen)
+uuidpassword=$(uuidgen)
+
+echo "rpcuser=$uuiduser" >> /guldenserver/datadir/Gulden.conf
+echo "rpcpassword=$uuidpassword" >> /guldenserver/datadir/Gulden.conf
