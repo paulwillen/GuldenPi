a16b2b065e911fa881de55583a1b1ba9bf8030c4
Gerben D <geppyz@users.noreply.github.com>
2017-10-09 18:37:13 +0200

    Added an upgrade script for Rasbian
    Run this as root

---
+++ bash/upgrade-rasbias.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Stop the running GuldenD Service
+echo "[$(date)] Stopping GuldenD service" 
+systemctl stop GuldenD.service
+echo "[$(date)] GuldenD service stopped" 
+
+############ Download & install Gulden Software Update ######
+
+echo "[$(date)] Removing old Gulden download" 
+rm -f /guldenserver/*.tar.gz
+echo "[$(date)] Downloading new Gulden software from github" 
+wget https://github.com/Gulden/gulden-official/releases/download/v1.6.4.3/Gulden-1.6.4.3-arm-linux-eabihf.tar.gz -P /guldenserver
+echo "[$(date)] Download completed, now unpacking Gulden software" 
+tar -xvf /guldenserver/Gulden-1.6.4.3-arm-linux-eabihf.tar.gz -C /guldenserver/
+
+# Start the GuldenD Service
+echo "[$(date)] Unpacking done, starting up GuldenD service" 
+systemctl start GuldenD.service
+echo "[$(date)] GuldenD service started, upgrade complete"  
+exit
