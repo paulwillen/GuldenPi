194ece22f7c89ffef46dbecd89879e41bae6875e
Gerben D <geppyz@users.noreply.github.com>
2017-10-21 16:39:08 +0200

    Updated to install Gulden 1.6.4.6

--- bash/upgrade-raspbian.sh
+++ bash/upgrade-raspbian.sh
@@ -10,9 +10,9 @@ echo "[$(date)] GuldenD service stopped"
 echo "[$(date)] Removing old Gulden download" 
 rm -f /guldenserver/*.tar.gz
 echo "[$(date)] Downloading new Gulden software from github" 
-wget https://github.com/Gulden/gulden-official/releases/download/v1.6.4.3/Gulden-1.6.4.3-arm-linux-eabihf.tar.gz -P /guldenserver
+wget https://github.com/Gulden/gulden-official/releases/download/v1.6.4.6/Gulden-1.6.4.6-arm-linux-eabihf.tar.gz -P /guldenserver
 echo "[$(date)] Download completed, now unpacking Gulden software" 
-tar -xvf /guldenserver/Gulden-1.6.4.3-arm-linux-eabihf.tar.gz -C /guldenserver/
+tar -xvf /guldenserver/Gulden-1.6.4.6-arm-linux-eabihf.tar.gz -C /guldenserver/
 
 # Start the GuldenD Service
 echo "[$(date)] Unpacking done, starting up GuldenD service" 
